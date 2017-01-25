159180635f1ab5d7d7f653cee9753354d850ef8e
Haris Khan <perfekttechblog@gmail.com>
2017-01-25 00:41:59 -0500

    ESLint-Watch with reccomended settings

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 //debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`); //backticks tell ES6 to parse variables in string
+console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+//backticks tell ES6 to parse variables in string
